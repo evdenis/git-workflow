91a1e8b3024bc4cb290f8a3f22892bff435a9b5c
Denis Efremov <yefremov.denis@gmail.com>
2015-03-27 18:41:44 +0300

    short_commit_info

--- lib/Git/WorkFlow/AstraVer/Commit.pm
+++ lib/Git/WorkFlow/AstraVer/Commit.pm
@@ -5,11 +5,16 @@ use strict;
 
 use Exporter qw/import/;
 
-our @EXPORT_OK = qw/commit_info/;
+our @EXPORT_OK = qw/commit_info short_commit_info/;
 
 sub commit_info
 {
    $_[0]->run('log' => '-n1' => '--oneline' => $_[1])
+}
+
+sub short_commit_info
+{
+   $_[0]->run('log' => '-n1' => '--format=%s' => $_[1]);
 }
 
 1;
