8a5a469eab0ee0606c0a297642cf37bd7548658e
Denis Efremov <yefremov.denis@gmail.com>
2015-05-08 17:34:27 +0300

    get_root_dir, get_git_dir

--- lib/Git/WorkFlow/AstraVer/Repo.pm
+++ lib/Git/WorkFlow/AstraVer/Repo.pm
@@ -7,7 +7,7 @@ use Exporter qw/import/;
 
 use Git::Repository::Command;
 
-our @EXPORT_OK = qw/check_changes_exist/;
+our @EXPORT_OK = qw/check_changes_exist get_root_dir get_git_dir/;
 
 
 sub check_changes_exist
@@ -17,7 +17,18 @@ sub check_changes_exist
    my $cmd = Git::Repository::Command->new( $_[0], qw/diff-index --quiet HEAD --/);
    $cmd->close();
 
-   $cmd->exit();
+   $cmd->exit()
 }
+
+sub get_root_dir
+{
+   substr($_[0]->run(qw/rev-parse --git-dir/), 0, -5)
+}
+
+sub get_git_dir
+{
+   $_[0]->run(qw/rev-parse --git-dir/)
+}
+
 
 1;
